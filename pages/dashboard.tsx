24ac5539c6ef1fe517db01fdaf5321ef776f06d2
plotchy <plotchy@tutanota.com>
2024-07-26 16:30:12 -0400

    add faq in warning

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -266,7 +266,7 @@ export default function DashboardPage() {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
           </svg>
           <span className="text-yellow-800 font-semibold">
-            Warning: This site only works while this tab is active and visible. Keep your pc awake and this tab open and visible to ensure your Kamis are cared for!
+            Warning: This site only works while this tab is active and visible. Keep your pc awake and this tab open and visible to ensure your Kamis are cared for! Check the FAQ for details.
           </span>
         </div>
       </div>
